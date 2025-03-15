ba7d4027cd1e89ea401de8c5cae9ad93923e5860
travis ueki <travis@superfilter.ai>
2025-03-15 13:29:47 -0500

    fix deploy more

---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import Phaser from 'phaser';
+import '../style.css';
+import { MainGame } from './game';
+
+// Start the game
+new MainGame();
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  // Base path will be overridden by --base flag when provided
+  base: '/',
   server: {
     open: true,
     port: 3000
